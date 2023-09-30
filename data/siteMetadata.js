cf13b2ef9d94874f43e323835f36d0641af681dc
Stephan Lamoureux <sa.lamoureux@gmail.com>
2023-09-30 14:15:58 -0400

    added prettier config

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,22 +1,22 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
+  title: 'Stephan Lamoureux',
+  author: 'Stephan Lamoureux',
+  headerTitle: 'whereSteveCodes',
   description: 'A blog created with Next.js and Tailwind.css',
   language: 'en-us',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
+  siteUrl: 'where-steve-codes.vercel.app/',
   siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
   siteLogo: '/static/images/logo.png',
   socialBanner: '/static/images/twitter-card.png',
   mastodon: 'https://mastodon.social/@mastodonuser',
-  email: 'address@yoursite.com',
-  github: 'https://github.com',
+  email: 'sa.lamoureux@gmail.com',
+  github: 'https://github.com/stephanlamoureux',
   twitter: 'https://twitter.com/Twitter',
   facebook: 'https://facebook.com',
   youtube: 'https://youtube.com',
-  linkedin: 'https://www.linkedin.com',
+  linkedin: 'https://www.linkedin.com/in/stephanlamoureux',
   locale: 'en-US',
   analytics: {
     // If you want to use an analytics provider you have to add it to the
